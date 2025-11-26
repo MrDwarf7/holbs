8e9e994d3087671e66e3d71d7e26a4aa56a99c39
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-11-26 23:01:14 +1100

    fix: const fixes this

--- behavior.js
+++ behavior.js
@@ -1,8 +1,8 @@
-var thumbnailElement = document.getElementById("smart_thumbnail");
 
 document.addEventListener("DOMContentLoaded", function(event) {
   // JavaScript code goes here
   // alert("Hello!")
+  var thumbnailElement = document.getElementById("smart_thumbnail");
 
   thumbnailElement.addEventListener("click", function() {
     thumbnailElement.className = (thumbnailElement.className === "small") ? "" : "small";
