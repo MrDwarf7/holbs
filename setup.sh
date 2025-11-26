cb409384ebc5a88cf3c3a082f32642b5d80eb174
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-11-26 21:37:55 +1100

    chore: add aliases

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/env bash
 E_CWD="$(pwd)"
 
+aliases=$(
+  cat <<-'EOF'
+  alias l="ls -lah --color=always"
+  alias ls="ls -lah --color=always"
+  alias cls="clear"
+  alias ca="clear; ls -lah --color=always"
+  alias vim="nvim"
+EOF
+)
+
+echo "${aliases}" >>"${HOME}/.bashrc"
+echo "Aliases added to .bashrc"
+
 apt install -y neovim htop
