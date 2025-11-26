293906035e10cad6fefac8b8660283e0fea7c390
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-11-26 23:00:09 +1100

    fix: js requires on-mount

--- behavior.js
+++ behavior.js
@@ -1,10 +1,11 @@
 var thumbnailElement = document.getElementById("smart_thumbnail");
 
-thumbnailElement.addEventListener("click", function() {
-  thumbnailElement.className = (thumbnailElement.className === "small") ? "" : "small";
-})
-
 document.addEventListener("DOMContentLoaded", function(event) {
   // JavaScript code goes here
   // alert("Hello!")
+
+  thumbnailElement.addEventListener("click", function() {
+    thumbnailElement.className = (thumbnailElement.className === "small") ? "" : "small";
+  });
+
 });
