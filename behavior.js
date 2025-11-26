e2d4656cd17bbf647f8604a7be79e98424e08776
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-11-26 22:49:42 +1100

    chore: add smart_thumbnail

--- behavior.js
+++ behavior.js
@@ -1,4 +1,10 @@
+var thumbnailElement = document.getElementById("smart_thumbnail");
+
+thumbnailElement.addEventListener("click", function() {
+  thumbnailElement.className = (thumbnailElement.className === "small") ? "" : "small";
+})
+
 document.addEventListener("DOMContentLoaded", function(event) {
   // JavaScript code goes here
-  alert("Hello!")
+  // alert("Hello!")
 });
